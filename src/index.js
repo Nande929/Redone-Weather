2a9eebd4d1a7617a805801ed05074a8ed5ce4c59
Nande929 <nandenqeto9@gmail.com>
2025-09-24 11:32:35 +0200

    Added additional functionality

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 function showTemperature (response) {
 
+    //Displaying The data from the API on the page
     let city = document.querySelector("#cityName");
     let temperature = document.querySelector("#temp");
+    let weatherCondition = document.querySelector("#weatherCondition");
+    let humidity = document.querySelector("#humidity");
+    let windSpeed = document.querySelector("#wind-speed");
+    let realDate = document.getElementById("myDate");
+    let date = new Date(response.data.time * 1000);
+    let iconWeather = document.querySelector("#image-icon");
+
 
     city.innerHTML = response.data.city;
     temperature.innerHTML = `${Math.round(response.data.temperature.current)}°C`;
+    weatherCondition.innerHTML = response.data.condition.description;
+    humidity.innerHTML = `${response.data.temperature.humidity}`;
+    windSpeed.innerHTML = `${response.data.wind.speed}`;
+    realDate.innerHTML = `${currentDate(date)}`;
+    iconWeather.innerHTML = `<img src="${response.data.condition.icon_url}">` 
+    
 }
 
 function requestApi(city) {
+
+    //Gathering/requesting information from the API
 
     let apiKey = "e2b85d39ocbf2atcfebaabf3b2422057"
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`
@@ -16,6 +32,8 @@ function requestApi(city) {
 }
 
 function displayWeather(event) {
+
+    //Taking in the value/city the user types passing the value to the requestApi() function
     event.preventDefault();
 
    let searchInput = document.getElementById("search-form-input");
@@ -27,14 +45,20 @@ weatherForm.addEventListener("submit", displayWeather);
 
 function currentDate(date) {
 
+    //Accurate dates depending on city time zones
+
     let minutes = date.getMinutes();
     let hours = date.getHours();
     let day = date.getDay();
 
-    if (minutes < 10 && hours < 10) {
+    if (minutes < 10) {
 
         minutes = `0${minutes}`;
-        hours = `0${hours}`;
+    }
+
+    if (hours < 10) {
+
+        hours = `0${hours}`
     }
 
     let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
@@ -42,7 +66,6 @@ function currentDate(date) {
     return `${wordedDays} ${hours}:${minutes}`
 }
 
-let realDate = document.getElementById("myDate");
-let weatherDate = new Date();
 
-realDate.innerHTML = currentDate(weatherDate);
+
+requestApi("Rome")
